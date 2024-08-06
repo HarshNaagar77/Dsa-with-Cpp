bfc9f117506ef75911783aecdef1e8ba7f58877f
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-06 22:27:04 +0530

    Update

--- Stl/02_Vector/02_Vector.cpp
+++ Stl/02_Vector/02_Vector.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(){
 
-    vector<int> v1 ;            // it means vector v2 is created with default size of 2.
+    vector<int> v1 ;            // it means vector v2 is created with default size of 0.
     vector<int> v2(5 , 1) ;     // it means vector v2 is created with size of 5 and all values are initialised as 1.
 
     // operations on vector
@@ -28,5 +28,7 @@ int main(){
 
 
     v2.clear();
-    cout << "Vector size after clear : " << v2.size() << endl;
+    cout << "Vector size after clear : " << v2.size() << endl; 
+    cout << "Vector capacity after clear : " << v2.capacity() << endl;
+    // It only clears the values of array not the capacity . so , size = 0 and capacity is still default
 }
