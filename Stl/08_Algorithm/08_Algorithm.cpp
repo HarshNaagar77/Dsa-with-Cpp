fb0aba8e3f67bf168b1aa06170b51c6192e015b8
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-07 21:54:42 +0530

    Update

---
+++ Stl/08_Algorithm/08_Algorithm.cpp
@@ -0,0 +1,39 @@
+// Algorithm is a cpp stl which gives certin function to be done just by calling them 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+    vector< int > v ;
+    v.push_back(433);
+    v.push_back(32);
+    v.push_back(3);    
+    v.push_back(64);
+
+    // sort function is used to sort the vector
+    sort(v.begin() , v.end()) ;
+    for ( auto i : v ){
+        cout << i << ' ' ;
+    }
+    cout << endl ;
+
+    // binary_search function is used to search the element in the vector
+    cout << "Index of 64 :" << binary_search(v.begin() , v.end() , 64) << endl ;
+
+    // max and min 
+    int maxIt = *max_element(v.begin(), v.end());
+    cout << "Max element : " << maxIt << endl;
+
+    int minIt = *min_element(v.begin(), v.end());
+    cout << "Min element : " << minIt << endl;
+
+
+    // reverse function is used to reverse the vector
+    reverse(v.begin() , v.end());
+    for ( auto i  : v) {
+        cout << i << ' ' ;
+    }
+}
