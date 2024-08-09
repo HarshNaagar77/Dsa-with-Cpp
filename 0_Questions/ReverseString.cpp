dcded3383b28fcfbc6ae2706aa1f7cbda5540e0e
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-09 21:05:57 +0530

    Update

---
+++ 0_Questions/ReverseString.cpp
@@ -0,0 +1,20 @@
+// Reverse string 
+#include <iostream>
+#include <vector>
+using namespace std ;
+
+int main(){
+    string str ;
+    vector<char> v1;
+    
+    cout << "Enter Your text : ";
+    cin >> str ;
+    
+    for(int i = str.length() - 1 ; i >= 0 ; i-- ){
+        v1.push_back(str[i]);
+    }
+
+    for(int i = 0 ; i < v1.size() ; i++){
+        cout << v1[i] ;
+    }
+}
