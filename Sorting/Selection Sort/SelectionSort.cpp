1509e7d1ca411c51504ec4c5ac11645f4bebb093
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-14 20:39:24 +0530

    update

--- Sorting/Selection Sort/SelectionSort.cpp
+++ Sorting/Selection Sort/SelectionSort.cpp
@@ -5,7 +5,7 @@ int main(){
 
     int arr[5];
 
-    cout << "Enter 5 integers : " << endl;
+    cout << "Enter 5 integers ->  " << endl;
     
     for (int i = 0; i < 5; i++) {
         cin >> arr[i];
