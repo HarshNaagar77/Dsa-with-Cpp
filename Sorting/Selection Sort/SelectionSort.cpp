537a7fda06442d5af9a094e73129c72e87702362
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-14 20:38:42 +0530

    Update

--- Sorting/Selection Sort/SelectionSort.cpp
+++ Sorting/Selection Sort/SelectionSort.cpp
@@ -5,7 +5,7 @@ int main(){
 
     int arr[5];
 
-    cout << "Enter 5 integers:" << endl;
+    cout << "Enter 5 integers : " << endl;
     
     for (int i = 0; i < 5; i++) {
         cin >> arr[i];
@@ -15,6 +15,7 @@ int main(){
 
     for (int i = 0; i < arrSize - 1; i++){
         int minIndex = i;
+
 
         for (int j = i + 1; j < arrSize; j++){
             if (arr[j] < arr[minIndex]){
