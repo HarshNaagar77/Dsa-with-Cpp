970d911b207fe7089c40fc910d765084b8861797
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-15 16:14:35 +0530

    Update

--- Pointers/Concepts/03_Dynamic_memory_in _2d_array.cpp
+++ Pointers/Concepts/03_Dynamic_memory_in _2d_array.cpp
@@ -3,22 +3,24 @@ using namespace std ;
 
 int main(){
 
-    int n;
-    cout << "Enter n ";
+    int n , n2;
+    cout << "Enter no. of rows : ";
     cin >> n;
+    cout << "Enter no. of columns : " ;
+    cin >> n2 ;
 
 
     // create 2d array
     int **ptr = new int*[n];
     for(int i = i ; i <= n ; i++){
-        ptr[i] = new int[n];
+        ptr[i] = new int[n2];
     }
 
 
 
     // initialize 2d array
     for(int i = 1 ; i <= n ; i++){
-        for(int j = 1 ; j <= n ; j++){
+        for(int j = 1 ; j <= n2 ; j++){
             cout << "Enter element at position [" << i << "][" << j << "] ";
             cin >> ptr[i][j];
         }
@@ -27,7 +29,7 @@ int main(){
 
     // print 2d array
     for( int i = 1 ; i<=n ; i++){
-        for(int j = 1 ; j <= n ; j++){
+        for(int j = 1 ; j <= n2 ; j++){
             cout << ptr[i][j] << ' ';
         
         }
