c68582d8338efd730a02b72684baf9898a321bdb
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-16 15:30:05 +0530

    Update

---
+++ Recursion/04_Counting/Couting.cpp
@@ -0,0 +1,24 @@
+// counting using recursion 
+
+#include <iostream>
+using namespace std;
+
+int count(int n){
+
+    //base case
+    if(n == 0){
+        return 0;
+    }
+
+    count(n-1);            // recursive relation
+    cout << n << endl;
+    return 0;
+}
+
+int main(){
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+
+    int ans = count(n);
+}
