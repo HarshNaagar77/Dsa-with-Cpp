73b47fee3e3588c09d923438846eed457e6e5b6f
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-27 21:32:33 +0530

    Update

--- Recursion/05_Check_Sorted_Array.cpp/Array.cpp
+++ Recursion/05_Check_Sorted_Array.cpp/Array.cpp
@@ -8,20 +8,25 @@ int isSorted(int *arr , int size){
     }
     
     if( arr[0] > arr[1]){
-        cout << "array is not sorted";
+        return 0;
     }
     else{
        int ans =  isSorted(arr+1 , size-1);
-       cout << "array is sorted";
+       return ans;
 }
 }
  
 int main(){
 
-    int arr[5] = {2,4,55,9,11};
+    int arr[5] = {2,4,5,9,11};
     int size = 5 ;
 
     int ans = isSorted( arr , size);
-
+    if( ans == 0 ){
+        cout << "Array is not sorted" << endl;
+    }
+    else{
+        cout << "Array is sorted" << endl;
+    }
     
 }
