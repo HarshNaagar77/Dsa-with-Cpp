a3ddcba09ad0d0c874300f5ec25a79adfd61f1af
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2025-06-19 16:40:13 +0530

    Update

---
+++ 00_Questions/04_Valid palindrom/code.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int palindrome(string s){
+    int left = 0;
+    int right = s.length() - 1;
+
+    while(left < right){
+        while(left < right && !isalnum(s[left])){
+            left++;
+        }
+        while(left < right && !isalnum(s[right])){
+            right--;
+        }
+
+        if(tolower(s[left]) != tolower(s[right])){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main(){
+    string s = "A man, a plan, a canal: Panama";
+    if(palindrome(s)){
+        cout << "The string is a palindrome" << endl;
+    }
+    else{
+        cout << "The string is not a palindrome" << endl;
+    }
+}
