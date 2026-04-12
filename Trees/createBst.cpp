eae914d6090954b9343939a2f954bdec60aa681a
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2026-04-12 21:34:10 +0530

    update

--- Trees/createBst.cpp
+++ Trees/createBst.cpp
@@ -27,8 +27,44 @@ Node* buildTree( vector<int> v){
     return root;
 }
 
+void preorder(Node* root){
+    if( root == NULL){
+        return;
+    }
+    cout << root->val << endl;
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(Node* root){
+    if( root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout << root->val << endl;
+    inorder(root->right);
+}
+void postorder(Node* root){
+    if( root == NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->val << endl;
+}
+
 int main(){
     vector<int> v = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root =  buildTree(v);
-    cout << root->val ;
+    // cout << root->val ;
+    // cout << root->left->val ;
+    // cout << root->right->val ;
+    cout << "Preorder : " << endl ;
+    preorder(root);
+
+    cout << "Inorder : " << endl;
+    inorder(root);
+
+    cout << "Postorder : " << endl;
+    postorder(root);
+
 }
